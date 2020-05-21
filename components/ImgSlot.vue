d9d293de2d8dc46d899bac4f9e83a579353af321
Noriko <fujine@sria.co.jp>
2020-05-21 17:52:21 +0900

    スロット作成

---
+++ components/ImgSlot.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="imgBox">
+    <img
+      id="img1"
+      src="https://www.pakutaso.com/shared/img/thumb/017AME5031_TP_V.jpg"
+      alt="桜"
+      class="img"
+    />
+    <img
+      id="img2"
+      src="https://www.pakutaso.com/shared/img/thumb/PP_englishrose_TP_V.jpg"
+      alt="薔薇"
+      class="img"
+    />
+    <img
+      id="img3"
+      src="https://www.pakutaso.com/shared/img/thumb/AME832_teibanmomiji_TP_V.jpg"
+      alt="紅葉"
+      class="img"
+    />
+  </div>
+</template>
+
+<style>
+.imgBox {
+  display: flex;
+  justify-content: center;
+}
+.img {
+  width: 32.5%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <img-slot />
+    <input id="start" type="button" value="Start" @click="startSlot" />
+    <input id="stop" type="button" value="Stop" @click="stopSlot" />
+  </div>
+</template>
+
+<script>
+import ImgSlot from '~/components/ImgSlot.vue'
+
+export default {
+  components: {
+    ImgSlot
+  },
+  data() {
+    return {
+      imgList: [
+        'https://www.pakutaso.com/shared/img/thumb/017AME5031_TP_V.jpg',
+        'https://www.pakutaso.com/shared/img/thumb/PP_englishrose_TP_V.jpg',
+        'https://www.pakutaso.com/shared/img/thumb/AME832_teibanmomiji_TP_V.jpg'
+      ],
+      slotId: 1
+    }
+  },
+  methods: {
+    setImg(id) {
+      const selectImg = Math.floor(Math.random() * this.imgList.length)
+      document.getElementById(`${id}`).src = this.imgList[selectImg]
+    },
+    startSlot() {
+      const self = this
+      this.slotId = setInterval(function() {
+        self.setImg('img1')
+        self.setImg('img2')
+        self.setImg('img3')
+      }, 50)
+    },
+    stopSlot() {
+      clearInterval(this.slotId)
+    }
+  }
+}
+</script>
